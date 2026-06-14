d86aa9e2fffce972bd9971244945c8d03f838257
amidevous2 <amidevous+2@gmail.com>
2026-06-14 15:55:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,12 @@
 rm -rf $HOME/.build-scripts/root-.local/*
 chmod +x ./setup-cacerts.sh
 ./setup-cacerts.sh
+chmod +x ./setup-wget.sh
+./setup-wget.sh
 chmod +x 
+
+chmod +x 
+
+chmod +x 
+
 chmod +x 
