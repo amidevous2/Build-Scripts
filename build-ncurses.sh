e6593bf79617c17241dc86c3357c5c6b7bfed3dd
amidevous2 <amidevous+2@gmail.com>
2026-06-14 14:38:40 +0200

    Update build-ncurses.sh

--- build-ncurses.sh
+++ build-ncurses.sh
@@ -60,7 +60,7 @@ fi
 
 ###############################################################################
 
-if ! ./build-cacert.sh
+if ! ./build.sh cacert
 then
     echo "Failed to install CA Certs"
     exit 1
