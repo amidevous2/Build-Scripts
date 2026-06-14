f29cf925dea4c326b3381f76c5242f06c7510af3
amidevous2 <amidevous+2@gmail.com>
2026-06-14 13:00:52 +0200

    Update reset-repo.sh

--- reset-repo.sh
+++ reset-repo.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 cd $HOME/Build-Scripts 
 if [ -f $HOME/reset-build-scripts-repo.sh ]; then
+   bash $HOME/reset-build-scripts-repo.sh
+else
    echo "cd $HOME" > $HOME/reset-build-scripts-repo.sh
    echo "rm -rf Build-Scripts" >> $HOME/reset-build-scripts-repo.sh
    echo "git clone https://github.com/amidevous2/Build-Scripts.git" >> $HOME/reset-build-scripts-repo.sh
    echo "cd $HOME/Build-Scripts" >> $HOME/reset-build-scripts-repo.sh
-   echo "chmod +x *" >> $HOME/reset-build-scripts-repo.sh
+   bash $HOME/reset-build-scripts-repo.sh
 fi
-chmod +x $HOME/reset-build-scripts-repo.sh
-$HOME/reset-build-scripts-repo.sh
