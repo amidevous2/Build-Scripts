6f166aa0f280d7404efed7b5cdbc2de6536f6383
amidevous2 <amidevous+2@gmail.com>
2026-06-15 01:02:33 +0200

    Update build-base.sh

--- build-base.sh
+++ build-base.sh
@@ -149,6 +149,105 @@ echo "build idn2 work"
 sleep 10
 ###############################################################################
 
+if ! ./build.sh zlib
+
+then
+    echo "Failed to build zlib"
+    exit 1
+fi
+
+echo "build zlib work"
+sleep 10
+###############################################################################
+
+if ! ./build.sh bzip
+
+then
+    echo "Failed to build bzip"
+    exit 1
+fi
+
+echo "build bzip work"
+sleep 10
+###############################################################################
+
+if ! ./build.sh unistr
+
+then
+    echo "Failed to build unistr"
+    exit 1
+fi
+
+echo "build unistr work"
+sleep 10
+###############################################################################
+
+if ! ./build.sh bison
+
+then
+    echo "Failed to build bison"
+    exit 1
+fi
+
+echo "build bison work"
+sleep 10
+###############################################################################
+
+if ! ./build.sh gdbm
+
+then
+    echo "Failed to build gdbm"
+    exit 1
+fi
+
+echo "build gdbm work"
+sleep 10
+###############################################################################
+
+if ! ./build.sh bdb
+
+then
+    echo "Failed to build bdb"
+    exit 1
+fi
+
+echo "build bdb work"
+sleep 10
+###############################################################################
+
+if ! ./build.sh sed
+
+then
+    echo "Failed to build sed"
+    exit 1
+fi
+
+echo "build sed work"
+sleep 10
+###############################################################################
+
+if ! ./build.sh perl
+
+then
+    echo "Failed to build perl"
+    exit 1
+fi
+
+echo "build perl work"
+sleep 10
+###############################################################################
+
+if ! ./build.sh openssl
+
+then
+    echo "Failed to build openssl"
+    exit 1
+fi
+
+echo "build bzip work"
+sleep 10
+###############################################################################
+
 if ! ./build.sh wget
 
 then
