2002f6708ecd08b41bc52d6f8b1f2b387c23766f
amidevous2 <amidevous+2@gmail.com>
2026-06-14 14:47:57 +0200

    Update build-readline.sh

--- build-readline.sh
+++ build-readline.sh
@@ -36,7 +36,7 @@ fi
 
 ###############################################################################
 
-if ! ./build-cacert.sh
+if ! ./build.sh cacert
 then
     echo "Failed to install CA Certs"
     exit 1
