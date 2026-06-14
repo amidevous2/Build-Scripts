55215d42d506e5c43756d6357e0b749d3b311fa1
amidevous2 <amidevous+2@gmail.com>
2026-06-14 16:26:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
+cd $HOME/
+cat > $HOME/setup-build-scripts-repo.sh <<EOF
+#!/usr/bin/env bash
 cd $HOME
+mkdir -p Build-Scripts
+git clone https://github.com/amidevous2/Build-Scripts.git -b php56 Build-Scripts2
+rm -rf $HOME/Build-Scripts/*
+cp -R $HOME/Build-Scripts2/* $HOME/Build-Scripts/
+rm -rf $HOME/Build-Scripts2/
 rm -rf $HOME/.build-scripts/*
-rm -rf Build-Scripts
-git clone https://github.com/amidevous2/Build-Scripts.git -b php56 Build-Scripts
 cd $HOME/Build-Scripts
-chmod +x ./setup-cacerts.sh
+chmod +x *
 ./setup-cacerts.sh
-chmod +x ./setup-wget.sh
 ./setup-wget.sh
-chmod +x ./setup-bash.sh
 ./setup-bash.sh
-chmod +x ./build-base.sh
-chmod +x./build.sh wget
 INSTX_PREFIX="$HOME/.local"
 export INSTX_PREFIX
 ./build-base.sh
+EOF
+chmod +x $HOME/setup-build-scripts-repo.sh
+bash $HOME/setup-build-scripts-repo.sh
