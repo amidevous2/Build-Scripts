6c4da0eb7bd99c1b5a0ecff1e8ecdd5510f6a93a
amidevous2 <amidevous+2@gmail.com>
2026-06-15 16:55:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ script=$1
 minfolder=$(echo "$script" | cut -c -1)
 chmod +x build/$minfolder/$script.sh
 build/$minfolder/$script.sh
+
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+hash -r
