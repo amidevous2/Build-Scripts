c39d5a4e90792ad55ae28c4cf554bbc0913073b7
amidevous2 <amidevous+2@gmail.com>
2026-08-20 13:54:18 +0200

    Update gmp.sh

--- build/g/gmp.sh
+++ build/g/gmp.sh
@@ -3,11 +3,13 @@
 # Written and placed in public domain by Jeffrey Walton
 # This script builds GMP from sources.
 
-GMP_VER=6.2.0
-GMP_TAR="gmp-${GMP_VER}.tar.bz2"
-GMP_DIR="gmp-${GMP_VER}"
-PKG_NAME=gmp
 
+PKG_NAME=gmp
+PKG_VER=6.2.0
+PKG_TAR="${PKG_NAME}-${PKG_VER}.tar.bz2"
+PKG_DIR="${PKG_NAME}-${PKG_VER}"
+PKG_URL="https://ftp.gnu.org/gnu/gmp"
+echo "${PKG_NAME} ${PKG_VER} ${PKG_TAR} ${PKG_DIR} ${PKG_URL}"
 ###############################################################################
 
 # Get the environment as needed.
@@ -45,9 +47,9 @@ fi
 ###############################################################################
 
 echo ""
-echo "========================================"
-echo "================= GMP =================="
-echo "========================================"
+echo "=================================================="
+echo "============ ${PKG_NAME} v${PKG_VER} ============="
+echo "=================================================="
 
 echo ""
 echo "***********************"
@@ -55,10 +57,10 @@ echo "Downloading package"
 echo "***********************"
 
 echo ""
-echo "GMP ${GMP_VER}..."
+echo "GMP v${PKG_VER}..."
 
-if ! "${WGET}" -q -O "$GMP_TAR" \
-     "https://ftp.gnu.org/gnu/gmp/$GMP_TAR"
+if ! "${WGET}" -q -O "${PKG_TAR}" \
+     "${PKG_URL}/${PKG_TAR}"
 then
     echo ""
     echo "***********************"
@@ -67,59 +69,59 @@ then
     exit 1
 fi
 
-rm -rf "$GMP_DIR" &>/dev/null
-bzip2 -d < "$GMP_TAR" | tar xf -
-cd "$GMP_DIR" || exit 1
+rm -rf "${PKG_DIR}" &>/dev/null
+bzip2 -d < "${PKG_TAR}" | tar xf -
+cd "${PKG_DIR}" || exit 1
 
-if [[ -e ../patch/gmp.patch ]]; then
-    echo ""
-    echo "*************************"
-    echo "Patching package"
-    echo "*************************"
-
-    patch -u -p0 < ../patch/gmp.patch
-fi
+#if [[ -e ../patch/gmp.patch ]]; then
+#    echo ""
+#    echo "*************************"
+#    echo "Patching package"
+#    echo "*************************"
+#
+#    patch -u -p0 < ../patch/gmp.patch
+#fi
 
 # Fix decades old compile and link errors on early Darwin.
 # https://gmplib.org/list-archives/gmp-bugs/2009-May/001423.html
-if [[ "$OSX_10p5_OR_BELOW" -ne 0 ]]; then
-    if [[ -e ../patch/gmp-darwin.patch ]]; then
-        echo ""
-        echo "*************************"
-        echo "Patching package (Darwin)"
-        echo "*************************"
-
-        patch -u -p0 < ../patch/gmp-darwin.patch
-    fi
-fi
+#if [[ "$OSX_10p5_OR_BELOW" -ne 0 ]]; then
+#    if [[ -e ../patch/gmp-darwin.patch ]]; then
+#        echo ""
+#        echo "*************************"
+#        echo "Patching package (Darwin)"
+#        echo "*************************"
+#
+#        patch -u -p0 < ../patch/gmp-darwin.patch
+#    fi
+#fi
 
 # Fix sys_lib_dlsearch_path_spec
 bash "${INSTX_TOPDIR}/fix-configure.sh"
 
 # Fix FreeBSD configure test
-if true; then
-    file=configure
-    touch -a -m -r "$file" "$file.timestamp"
-    chmod a+w "$file"
-    sed 's/__builtin_clzl//g' "$file" > "$file.fixed"
-    mv "$file.fixed" "$file"
-    chmod a+x "$file"; chmod go-w "$file"
-    touch -a -m -r "$file.timestamp" "$file"
-    rm -f "$file.timestamp" "$file.fixed"
-fi
+#if true; then
+#    file=configure
+#    touch -a -m -r "$file" "$file.timestamp"
+#    chmod a+w "$file"
+#    sed 's/__builtin_clzl//g' "$file" > "$file.fixed"
+#    mv "$file.fixed" "$file"
+#    chmod a+x "$file"; chmod go-w "$file"
+#    touch -a -m -r "$file.timestamp" "$file"
+#    rm -f "$file.timestamp" "$file.fixed"
+#fi
 
 echo ""
 echo "***********************"
 echo "Configuring package"
 echo "***********************"
 
-if [[ "${INSTX_DEBUG_MAP}" -eq 1 ]]; then
-    gmp_cflags="${INSTX_CFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${GMP_DIR}"
-    gmp_cxxflags="${INSTX_CXXFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${GMP_DIR}"
-else
+#if [[ "${INSTX_DEBUG_MAP}" -eq 1 ]]; then
+#    gmp_cflags="${INSTX_CFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${GMP_DIR}"
+#    gmp_cxxflags="${INSTX_CXXFLAGS} -fdebug-prefix-map=${PWD}=${INSTX_SRCDIR}/${GMP_DIR}"
+#else
     gmp_cflags="${INSTX_CFLAGS}"
     gmp_cxxflags="${INSTX_CXXFLAGS}"
-fi
+#fi
 
 CONFIG_OPTS=()
 CONFIG_OPTS+=("--enable-static")
@@ -206,11 +208,11 @@ MAKE_FLAGS=("install")
 if [[ -n "${SUDO_PASSWORD}" ]]; then
     printf "%s\n" "${SUDO_PASSWORD}" | sudo ${SUDO_ENV_OPT} -S "${MAKE}" "${MAKE_FLAGS[@]}"
     printf "%s\n" "${SUDO_PASSWORD}" | sudo ${SUDO_ENV_OPT} -S bash "${INSTX_TOPDIR}/fix-permissions.sh" "${INSTX_PREFIX}"
-    printf "%s\n" "${SUDO_PASSWORD}" | sudo ${SUDO_ENV_OPT} -S bash "${INSTX_TOPDIR}/copy-sources.sh" "${PWD}" "${INSTX_SRCDIR}/${GMP_DIR}"
+    printf "%s\n" "${SUDO_PASSWORD}" | sudo ${SUDO_ENV_OPT} -S bash "${INSTX_TOPDIR}/copy-sources.sh" "${PWD}" "${INSTX_SRCDIR}/${PKG_DIR}"
 else
     "${MAKE}" "${MAKE_FLAGS[@]}"
     bash "${INSTX_TOPDIR}/fix-permissions.sh" "${INSTX_PREFIX}"
-    bash "${INSTX_TOPDIR}/copy-sources.sh" "${PWD}" "${INSTX_SRCDIR}/${GMP_DIR}"
+    bash "${INSTX_TOPDIR}/copy-sources.sh" "${PWD}" "${INSTX_SRCDIR}/${PKG_DIR}"
 fi
 
 ###############################################################################
@@ -221,13 +223,8 @@ cd "${CURR_DIR}" || exit 1
 
 ###############################################################################
 
-# Set to false to retain artifacts
-if true;
-then
-    ARTIFACTS=("$GMP_TAR" "$GMP_DIR")
-    for artifact in "${ARTIFACTS[@]}"; do
-        rm -rf "$artifact"
-    done
-fi
+echo "${PKG_NAME} ${PKG_VER} ${PKG_TAR} ${PKG_DIR} ${PKG_URL}"
+rm -rf "${PKG_DIR}" "${PKG_TAR}"
+
 
 exit 0
