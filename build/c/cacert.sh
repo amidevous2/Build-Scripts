b6ffac20c5804a90a54eae82350bf89d6d31c1f4
amidevous2 <amidevous+2@gmail.com>
2026-08-21 12:18:51 +0200

    Update cacert.sh

--- build/c/cacert.sh
+++ build/c/cacert.sh
@@ -26,6 +26,12 @@ if [[ "${SUDO_PASSWORD_DONE}" != "yes" ]]; then
     fi
 fi
 
+if [[ -e "${INSTX_PKG_CACHE}/${PKG_NAME}" ]]; then
+    echo ""
+    echo "$PKG_NAME is already installed."
+    exit 0
+fi
+
 ###############################################################################
 
 # setup-cacert.sh writes the certs locally for the user so we
