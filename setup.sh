b0fa9b9580362e628eb4c699860b2345c9aaf075
amidevous2 <amidevous+2@gmail.com>
2026-08-21 11:57:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,21 +4,25 @@ cat > $HOME/setup-build-scripts-repo.sh <<EOF
 #!/usr/bin/env bash
 cd $HOME
 mkdir -p Build-Scripts
-git clone https://github.com/amidevous2/Build-Scripts.git -b php56 Build-Scripts2
+DL="wget --no-check-certificate -qO-"
+COMMIT=\$(\$DL "https://api.github.com/repos/amidevous2/Build-Scripts/commits/php56" | jq -r .sha)
+mkdir -p Build-Scripts
+wget https://github.com/amidevous2/Build-Scripts/archive/\$COMMIT.tar.gz -O Build-Scripts.targ.gz
+tar -xvf Build-Scripts.targ.gz
 rm -rf $HOME/Build-Scripts/*
-cp -R $HOME/Build-Scripts2/* $HOME/Build-Scripts/
-rm -rf $HOME/Build-Scripts2/
-#rm -rf $HOME/.build-scripts/*
+cp -R $HOME/Build-Scripts-\$COMMIT/* $HOME/Build-Scripts/
+rm -rf $HOME/Build-Scripts-\$COMMIT/ Build-Scripts.targ.gz
 cd $HOME/Build-Scripts
 chmod +x *
-if [ ! -d $HOME/.build-scripts/]; then
+EOF
+rm -rf $HOME/.build-scripts/
 ./setup-cacerts.sh
 ./setup-wget.sh
 ./setup-bash.sh
-fi
 INSTX_PREFIX="$HOME/.local"
 export INSTX_PREFIX
-./build-base.sh
+PATH=$HOME/.build-scripts/wget/bin:$HOME/.local/bin
+#./build-base.sh
 EOF
 chmod +x $HOME/setup-build-scripts-repo.sh
 bash $HOME/setup-build-scripts-repo.sh
